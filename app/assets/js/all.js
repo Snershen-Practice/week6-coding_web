4e09afefaca6976dfa436dff3c6ced9de0269d7f
Wu Jane <jane72418@gmail.com>
2022-09-07 10:35:57 +0800

    add swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,12 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 });
